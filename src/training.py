a26900d6eb66e91a26a48b63d0efbfbdd3dc810d
Kirill Zhankov <k.zhankov@student.tudelft.nl>
2025-05-11 00:32:49 +0200

    Move average_inhomogeneous function to utilities.py

--- src/training.py
+++ src/training.py
@@ -16,7 +16,7 @@ with app.setup:
     from torch import nn
     import torch.nn.functional as F
     import json
-    from utilities import TransformedDataset, CircularIterator
+    from utilities import TransformedDataset, CircularIterator, average_inhomogeneous
 
     DETERMINISTIC = False  # FIXME
     SEED = 42
@@ -524,39 +524,6 @@ def _(average_of, results_file, run_experiment, save_results_to_file):
         print("ALL EXPERIMENTS DONE!")
         return results
     return (run_experiments,)
-
-
-@app.function
-def average_inhomogeneous(xsss):
-    """
-    Average lists of the same shape along the first dimension.
-
-    Each element of xsss is a list xss such that len(xss[i]) is not necessarily equal to len(xss[j]) for i != j. However,
-    len(xsss[i]) must always be equal to len(xsss[j]) for any i, j.
-
-    This function returns a list of averages as the first element and 
-    a list of standard deviations as the second element, both of the same shape as xsss[i] for any i.
-
-    Example:
-    a = [[1, 2, 3], [10, 20, 30, 40], [100]]
-    b = [[4, 5, 6], [50, 60, 70, 80], [600]]
-
-    >>> average_inhomogeneous([a, b])[0]
-    [[2.5, 3.5, 4.5], [30, 40, 50, 60], [350]]
-    """
-    n_sublists = len(xsss[0])
-    avgs = []
-    stds = []
-
-    for i in range(n_sublists):
-        group = [xss[i] for xss in xsss]
-        stacked = np.stack(group, axis=0)
-        avg = stacked.mean(axis=0).tolist()
-        std = stacked.std(axis=0, ddof=0).tolist()
-        avgs.append(avg)
-        stds.append(std)
-
-    return avgs, stds
 
 
 @app.cell
--- src/utilities.py
+++ src/utilities.py
@@ -6,6 +6,7 @@ __generated_with = "0.13.1"
 app = marimo.App(width="medium")
 
 with app.setup:
+    import numpy as np
     import json
     from torch.utils.data import Dataset
 
@@ -51,6 +52,39 @@ class CircularIterator:
         value = self.items[self.index]
         self.index = (self.index + 1) % len(self.items)
         return value
+
+
+@app.function
+def average_inhomogeneous(xsss):
+    """
+    Average lists of the same shape along the first dimension.
+
+    Each element of xsss is a list xss such that len(xss[i]) is not necessarily equal to len(xss[j]) for i != j. However,
+    len(xsss[i]) must always be equal to len(xsss[j]) for any i, j.
+
+    This function returns a list of averages as the first element and 
+    a list of standard deviations as the second element, both of the same shape as xsss[i] for any i.
+
+    Example:
+    a = [[1, 2, 3], [10, 20, 30, 40], [100]]
+    b = [[4, 5, 6], [50, 60, 70, 80], [600]]
+
+    >>> average_inhomogeneous([a, b])[0]
+    [[2.5, 3.5, 4.5], [30, 40, 50, 60], [350]]
+    """
+    n_sublists = len(xsss[0])
+    avgs = []
+    stds = []
+
+    for i in range(n_sublists):
+        group = [xss[i] for xss in xsss]
+        stacked = np.stack(group, axis=0)
+        avg = stacked.mean(axis=0).tolist()
+        std = stacked.std(axis=0, ddof=0).tolist()
+        avgs.append(avg)
+        stds.append(std)
+
+    return avgs, stds
 
 
 if __name__ == "__main__":

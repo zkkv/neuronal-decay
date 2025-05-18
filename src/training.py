ab13908d4c2b9701f0256c38798b3253f12690d9
Kirill Zhankov <k.zhankov@student.tudelft.nl>
2025-05-19 01:11:25 +0200

    Move X and y to device

--- src/training.py
+++ src/training.py
@@ -239,71 +239,78 @@ class ExperimentResult:
         return f"{self.__class__.__name__}({attrs})"
 
 
-@app.function
-def compute_accuracy(model, dataset, test_size=None, batch_size=128):
-    '''
-    Compute accuracy (% samples classified correctly) of a classifier ([model]) on [dataset].
-    '''
+@app.cell
+def _(device):
+    def compute_accuracy(model, dataset, test_size=None, batch_size=128):
+        '''
+        Compute accuracy (% samples classified correctly) of a classifier ([model]) on [dataset].
+        '''
 
-    mode = model.training
-    model.eval()
+        mode = model.training
+        model.eval()
 
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=False)
-    total_tested = 0
-    total_correct = 0
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=False)
+        total_tested = 0
+        total_correct = 0
 
-    for X, y in dataloader:
-        if test_size and total_tested >= test_size:
-            break
-        with torch.no_grad():
-            scores, _ = model(X)
-        _, pred = torch.max(scores, 1)
-        total_correct += (pred == y).sum().item()
-        total_tested += len(X)
-    accuracy = total_correct * 100 / total_tested
-
-    model.train(mode=mode)
-
-    return accuracy
-
-
-@app.function
-def train_and_eval(model, train_set, n_batches, batch_size, task_idx, test_sets, test_size, performance, optimizer, loss_fn, decay_lambda):
-    '''
-    Function to train a [model] on a given [train_set],
-    while evaluating after each training iteration on [test_sets].
-    '''
-    model.train()
-    iters_left = 1
-    print_every_n = 25
-
-    dataloader = CircularIterator(DataLoader(train_set, batch_size=batch_size, shuffle=True))
-
-    for batch_idx in range(n_batches):
-
-        (X, y) = next(dataloader)
-
-        # Prediction
-        pred, decay = model(X)
-
-        # Evaluation
-        loss = loss_fn(pred, y) + decay_lambda * decay
-
-        for test_idx, test_set in enumerate(test_sets):
-            if test_idx >= task_idx:
+        for X, y in dataloader:
+            if test_size and total_tested >= test_size:
                 break
-            accuracy = compute_accuracy(model, test_set, test_size, batch_size)
-            performance[test_idx].append(accuracy)
 
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            with torch.no_grad():
+                scores, _ = model(X)
+            _, pred = torch.max(scores, 1)
+            total_correct += (pred == y).sum().item()
+            total_tested += len(X)
+        accuracy = total_correct * 100 / total_tested
 
-        # Logging
-        if batch_idx % print_every_n == 0: 
-            print('Training loss: {loss:04f} | Test accuracy: {prec:05.2f}% | Batch: {b_index}'
-                .format(loss=loss.item(), prec=accuracy, b_index=batch_idx))
+        model.train(mode=mode)
+
+        return accuracy
+    return (compute_accuracy,)
+
+
+@app.cell
+def _(compute_accuracy, device):
+    def train_and_eval(model, train_set, n_batches, batch_size, task_idx, test_sets, test_size, performance, optimizer, loss_fn, decay_lambda):
+        '''
+        Function to train a [model] on a given [train_set],
+        while evaluating after each training iteration on [test_sets].
+        '''
+        model.train()
+        iters_left = 1
+        print_every_n = 25
+
+        dataloader = CircularIterator(DataLoader(train_set, batch_size=batch_size, shuffle=True))
+
+        for batch_idx in range(n_batches):
+
+            X, y = next(dataloader)
+            X, y = X.to(device), y.to(device)
+        
+            # Prediction
+            pred, decay = model(X)
+
+            # Evaluation
+            loss = loss_fn(pred, y) + decay_lambda * decay
+
+            for test_idx, test_set in enumerate(test_sets):
+                if test_idx >= task_idx:
+                    break
+                accuracy = compute_accuracy(model, test_set, test_size, batch_size)
+                performance[test_idx].append(accuracy)
+
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            # Logging
+            if batch_idx % print_every_n == 0: 
+                print('Training loss: {loss:04f} | Test accuracy: {prec:05.2f}% | Batch: {b_index}'
+                    .format(loss=loss.item(), prec=accuracy, b_index=batch_idx))
+    return (train_and_eval,)
 
 
 @app.function
@@ -460,7 +467,7 @@ def _(
 
 
 @app.cell
-def _(average_of, n_batches_per_task, n_tasks, train_datasets):
+def _(average_of, n_batches_per_task, n_tasks, train_and_eval, train_datasets):
     def run_experiment(experiment, run):
         print(f" Running experiment {experiment.experiment_no} (run {run}/{average_of}) ".center(60, "~"))
         performance_history = [[] for _ in range(n_tasks)]

aa7a188b4313b2a1d117fad7f01924c2b964dbbf
Kirill Zhankov <k.zhankov@student.tudelft.nl>
2025-05-23 04:47:43 +0200

    Add import test

---
+++ utilities/import_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# This script allows you to test that all necessary dependencies are installed
+
+def test():
+	# Build-ins
+	import json
+	import os
+	import dataclasses
+
+	# External
+	import torch
+	import torchvision
+	import matplotlib
+	import numpy
+
+	print("All dependencies were found!")
+
+
+if __name__ == "__main__":
+	test()

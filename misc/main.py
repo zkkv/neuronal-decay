90dabd5a8e418224f5b46cf33c1cea9efe8b88fc
Kirill Zhankov <k.zhankov@student.tudelft.nl>
2025-06-09 08:59:54 +0200

    Add misc code for stability gap sketch

--- misc/main.py
+++ misc/main.py
@@ -1,9 +1,14 @@
 from .rotations import plot_rotations
+from .stability_gap import plot_stability_gap
+from utilities.fs import make_dirs
+from utilities.meta import MISC_DIR
 
 
 def main():
+	make_dirs([MISC_DIR])
 	format = "pdf"
-	plot_rotations(save_as=f"./out/misc/rotations.{format}")
+	plot_rotations(save_as=f"{MISC_DIR}/rotations.{format}")
+	plot_stability_gap(save_as=f"{MISC_DIR}/stability_gap.{format}")
 
 
 if __name__ == "__main__":
